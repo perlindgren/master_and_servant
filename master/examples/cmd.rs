fe701c3ccf254b71c41908545a1b949668f52dfb
Per Lindgren <per.lindgren@ltu.se>
2023-09-12 00:17:02 +0200

    cleanup

--- master/examples/cmd.rs
+++ master/examples/cmd.rs
@@ -9,7 +9,7 @@
 use master_and_servant::{Command, Message, Response};
 use serial2::SerialPort;
 use std::io::Read;
-use std::mem::{size_of, size_of_val};
+use std::mem::size_of;
 use master::open;
 
 type InBuf = [u8; size_of::<Command>()];
@@ -21,40 +21,37 @@ fn main() -> Result<(),  std::io::Error> {
     let mut out_buf = [0u8; size_of::<Command>()];
     let mut in_buf = [0u8; size_of::<Response>()];
 
+    let cmd = Command::Set(0x12, Message::B(12), 0b001);
+    println!("request {:?}", cmd);
     let response = request(
-        Command::Set(0x12, Message::B(12), 0b001),
+        &cmd,
         &mut port,
         &mut out_buf,
         &mut in_buf,
-    );
+    )?;
     println!("response {:?}", response);
+
+    let cmd = Command::Get(0x12, 12, 0b001);
+    println!("request {:?}", cmd);
     let response = request(
-        Command::Get(0x12, 12, 0b001),
+        &cmd,
         &mut port,
         &mut out_buf,
         &mut in_buf,
-    );
+    )?;
     println!("response {:?}", response);
     Ok(())
 }
 
 fn request(
-    cmd: Command,
+    cmd: &Command,
     port: &mut SerialPort,
     out_buf: &mut OutBuf,
     in_buf: &mut InBuf,
-) -> Response {
-    let n = ssmarshal::serialize(out_buf, &cmd).unwrap();
-    println!("cdm {:?}, size {}, n {}", cmd, size_of_val(&cmd), n);
-
-    let r = port.write_all(out_buf);
-    println!("{:?}", r);
-    let r = port.flush();
-    println!("{:?}", r);
-
-    let r = port.read_exact(in_buf);
-    println!("{:?}, {:?}", r, in_buf);
+) -> Result<Response, std::io::Error> {
+    let _n = ssmarshal::serialize(out_buf, cmd).unwrap();
+    port.write_all(out_buf)?;
+    port.read_exact(in_buf)?;
     let (response, _) = ssmarshal::deserialize::<Response>(in_buf).unwrap();
-
-    response
+    Ok(response)
 }
